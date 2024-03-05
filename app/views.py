998e2c5584d5bf3587f62a751f417db0c78dbef3
madhanmanu03 <madhanmanu03@gmail.com>
2024-03-05 22:03:43 +0530

    first commit\

--- app/views.py
+++ app/views.py
@@ -6,12 +6,13 @@ from app.serializers import *
 # Create your views here.
 
 class Productcrud(APIView):
-    def get(self,request):
+    def get(self,request,Product_Id):
         POD=Product.objects.all()
+        #POD=Product.objects.get(Product_Id=Product_Id)--->if we are giving get then we should not give many=True
         JPOD=ProductSerializers(POD,many=True)
         return Response(JPOD.data)
 
-    def post(self,request):
+    def post(self,request,Product_Id):
         JSO=request.data
         POD=ProductSerializers(data=JSO)
         if POD.is_valid():
@@ -19,3 +20,25 @@ class Productcrud(APIView):
             return Response({"insert":"INSERTED"})
         else:
             return Response({"Not":"NOT INSERTED"})
+
+    def put(self,request,Product_Id):
+        PO=Product.objects.get(Product_Id=Product_Id)
+        PDO=ProductSerializers(PO,data=request.data)
+        if PDO.is_valid():
+            PDO.save()
+            return Response({'update':'Data is Updated'})
+        else:
+            return Response({'Error':'Data is not Updated'})
+
+    def patch(self,request,Product_Id):
+        PO=Product.objects.get(Product_Id=Product_Id)
+        UPDO=ProductModelSerializer(PO,data=request.data,partial=True)
+        if UPDO.is_valid():
+            UPDO.save()
+            return Response({'update':'Data is Updated'})
+        else:
+            return Response({'error':'Update not done'})
+
+    def delete(self,request,Product_Id):
+        Product.objects.get(Product_Id=Product_Id).delete()
+        return Response({'deletion':'Data is Deleted'})
